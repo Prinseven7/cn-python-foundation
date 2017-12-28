96fc347a032aebb2c0d463155d08c7f229bfde8e
Prinseven7 <34733979+Prinseven7@users.noreply.github.com>
2017-12-27 21:48:41 -0800

    Update Task1.py

--- investigate texts and calls/EN/Task1.py
+++ investigate texts and calls/EN/Task1.py
@@ -43,8 +43,8 @@ flat_texts = []
 flat_calls =[]
 flat_texts = flat(texts)
 flat_calls = flat(calls)
-#print(flat_texts[:7])
-#print(flat_calls[:10])
+#print(flat_texts[:7])        #Check the result
+#print(flat_calls[:10])       #Check the result
 
 #Delete timestamp of text message, so only telephone numbers left
 def phone_number_texts(lists):
@@ -52,16 +52,16 @@ def phone_number_texts(lists):
     for number in range(len(lists)):
         if (number+1) % 3 == 0:              #Search timestamp elements in texts
             temp_list1.append(lists[number])
-    #print(temp_list1[:5])
+    #print(temp_list1[:5])    #Check the result
     for item in temp_list1:
         if item in lists:
             lists.remove(item)          #Delete timestamp elements in texts
-    #print(lists[:7])
-    #print(len(lists))
+    #print(lists[:7])         #Check the result
+    #print(len(lists))        #Check the result
     return lists
 x = phone_number_texts(flat_texts)
-#print(len(x))
-#print(x[:7])
+#print(len(x))                #Check the result
+#print(x[:7])                 #Check the result
 
 #Delete timestamp and duration of calls, so only telephone numbers left
 def phone_number_calls(lists):
@@ -70,28 +70,28 @@ def phone_number_calls(lists):
     for number in range(len(lists)):
         if (number+1) % 4 == 0:         #Search duration elements in calls
             temp_list2.append(lists[number])
-    #print(temp_list[:5])
+    #print(temp_list[:5])     #Check the result
     for item in temp_list2:
         if item in lists:
             lists.remove(item)         #Delete duration elements in calls
-    #print(temp_list2[:8])
+    #print(temp_list2[:8])    #Check the result
     for number in range(len(lists)):
         if (number+1) % 3 == 0:        #Search timestamp elements in calls
             temp_list3.append(lists[number])
-    #print(temp_list[:5])
+    #print(temp_list[:5])     #Check the result
     for item in temp_list3:
         if item in lists:
             lists.remove(item)        #Delete timestamp elements in calls
-    #print(lists[:7])
-    #print(len(lists))
+    #print(lists[:7])         #Check the result
+    #print(len(lists))        #Check the result
     return lists
 y = phone_number_calls(flat_calls)
-#print(len(y))
-#print(y[:10])
+#print(len(y))                #Check the result
+#print(y[:10])                #Check the result
 
 set_number_texts = set(x)          #Create set of all numbers in texts
 set_number_calls = set(y)          #Create set of all numbers in calls
-
+#print(len(set_number_calls)) #Check the result
 set_number_texts_calls = set_number_texts | set_number_calls     #Create aggregate set of two sets
 
 print("There are <{}> different telephone numbers in the records.".format(len(set_number_texts_calls)))
